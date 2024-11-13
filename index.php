e0c0953983bc4d6fb26c8be56a005675e077df87
huudungg <huudung2004kt@gmail.com>
2024-11-13 10:26:27 +0700

    loginPage

--- index.php
+++ index.php
@@ -16,48 +16,57 @@
         <label for="email">Email:</label><br>
         <input type="text" id="email" name="email"><br><br>
 
-        <label for="password">Mật khẩu:</label><br>
-        <input type="text" id="password" name="password"><br><br>
+        <label for="phone">password:</label><br>
+        <input type="text" id="phone" name="phone"><br><br>
 
-        <input type="submit" value="Đăng ký">
+        <input type="submit" name="register" value="Đăng ký">
+        <input type="submit" name="show" value="show json">
     </form>
 
     <?php
-    $error = [];
+    if ($_POST["register"]) {
+        $error = [];
 
-    if (empty($_POST["username"])) {
-        $error[] = "username must be not empty";
-    }
+        if (empty($_POST["username"])) {
+            $error[] = "username must be not empty";
+        }
 
-    if (empty($_POST["email"])) {
-        $error[] = "email must be not empty";
-    }
+        if (empty($_POST["email"])) {
+            $error[] = "email must be not empty";
+        }
 
-    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        $error[] = "email is invalid";
-    }
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            $error[] = "email is invalid";
+        }
 
-    if (empty($_POST["password"])) {
-        $error[] = "password must be not empty";
-    }
+        if (empty($_POST["phone"])) {
+            $error[] = "phone must be not empty";
+        }
 
-    if (strlen($_POST["password"]) < 8) {
-        $error[] = "password must be more then 8 char";
-    }
+        if (strlen($_POST["phone"]) < 8) {
+            $error[] = "phone must be more then 8 char";
+        }
 
-    if (empty($error)) {
-        $myfile = fopen("account.json", "w");
-        $data = json_encode(["username" => $_POST["username"], "email" => $_POST["email"], "password" => $_POST["password"]]);
-        fwrite($myfile, $data);
-    } else {
-        foreach ($error as $e) {
-            echo "<div>$e</div>";
+        if (empty($error)) {
+            $myfile = fopen("account.json", "w");
+            $data = json_encode(["username" => $_POST["username"], "email" => $_POST["email"], "phone" => $_POST["phone"]]);
+            fwrite($myfile, $data);
+            echo "register success";
+        } else {
+            foreach ($error as $e) {
+                echo "<div>$e</div>";
+            }
         }
     }
 
-
-
-
+    if (isset($_POST["show"])) {
+        if (file_exists("account.json")) {
+            $myfile = file_get_contents("account.json");
+            echo "show data: " . $myfile;
+        } else {
+            echo "File account.json không tồn tại.";
+        }
+    }
     ?>
 </body>
 
