d60367bc40d84299b4046577f7400164377b8118
huudungg <huudung2004kt@gmail.com>
2024-11-13 10:13:19 +0700

    userLogin

--- index.php
+++ index.php
@@ -1,23 +1,64 @@
-<?php
-$matrix = [
-    [1.2, 2.3, 3.5, 4.6],
-    [5.7, 6.8, 7.9, 8.1],
-    [9.2, 10.3, 11.4, 12.5]
-];
+<!DOCTYPE html>
+<html lang="vi">
 
-$maxValue = $matrix[0][0];
-$maxRow = 0;
-$maxCol = 0;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng ký người dùng</title>
+</head>
 
-for ($i = 0; $i < count($matrix); $i++) {
-    for ($j = 0; $j < count($matrix[$i]); $j++) {
-        if ($maxValue < $matrix[$i][$j]) {
-            $maxValue = $matrix[$i][$j];
-            $maxRow = $i;
-            $maxCol = $j;
+<body>
+    <h2>Đăng ký người dùng</h2>
+    <form action="" method="POST">
+        <label for="username">Tên đăng nhập:</label><br>
+        <input type="text" id="username" name="username"><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="text" id="email" name="email"><br><br>
+
+        <label for="password">Mật khẩu:</label><br>
+        <input type="text" id="password" name="password"><br><br>
+
+        <input type="submit" value="Đăng ký">
+    </form>
+
+    <?php
+    $error = [];
+
+    if (empty($_POST["username"])) {
+        $error[] = "username must be not empty";
+    }
+
+    if (empty($_POST["email"])) {
+        $error[] = "email must be not empty";
+    }
+
+    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $error[] = "email is invalid";
+    }
+
+    if (empty($_POST["password"])) {
+        $error[] = "password must be not empty";
+    }
+
+    if (strlen($_POST["password"]) < 8) {
+        $error[] = "password must be more then 8 char";
+    }
+
+    if (empty($error)) {
+        $myfile = fopen("account.json", "w");
+        $data = json_encode(["username" => $_POST["username"], "email" => $_POST["email"], "password" => $_POST["password"]]);
+        fwrite($myfile, $data);
+    } else {
+        foreach ($error as $e) {
+            echo "<div>$e</div>";
         }
     }
-}
 
-echo "max value is: " . $maxValue;
-echo "max matrix is: matrix[$maxRow, $maxCol]";
+
+
+
+    ?>
+</body>
+
+</html>
